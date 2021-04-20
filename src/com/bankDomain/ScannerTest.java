4b257bfd6c1ae3e3388af484b7fd6a2d7ccd9795
Rahul Belokar <rbelokar@rbelokar.pnq.csb>
2021-04-20 16:13:33 +0530

    initial commit

---
+++ src/com/bankDomain/ScannerTest.java
@@ -0,0 +1,13 @@
+package com.bankDomain;
+
+import java.util.Scanner;
+
+public class ScannerTest {
+	public static void main(String[] args) {
+		Scanner scanner =new Scanner(System.in);
+		int a = scanner.nextInt();
+		System.out.println("Value ="+a);
+		
+}	
+
+}
